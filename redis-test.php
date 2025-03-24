c134c5e70f7a2778c1a91b596599ea0f393e8859
novseje <novseje@gmail.com>
2025-03-24 10:27:10 +0200

    redis-test

--- redis-test.php
+++ redis-test.php
@@ -1,4 +1,10 @@
 <?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $redis=new Redis() or die("Can not load Redis.");
+$redis->auth('redis_password'); 
 $redis->connect('127.0.0.1'); 
 $redis->set('Redis test_key', 1);
